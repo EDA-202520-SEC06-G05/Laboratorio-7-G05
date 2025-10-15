cc4dade50039bf634de62cb9ef72d24314a6a1b5
San-FG <manuelsal5436-gmail.com>
2025-10-15 15:14:43 -0500

    Ingreso de la primera version de

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -1,3 +1,5 @@
+from DataStructures.Tree import bst_node 
+
 def new_map ():
     
     return {"root": None}
@@ -9,11 +11,23 @@ def default_compare (key, element):
         return 1
     return -1
 
-def get_nodo(root, key):
+def get_node(root, key):
     
-    
-    return
-    
+    if root["left"] == None and root["right"] == None:
+        if root["key"] == key:
+            return root 
+        else: 
+            return None
+        
+    elif root["left"] == None and root["right"] != None:
+        get_node(root["right"], key)
+    elif root["left"] != None and root["right"] == None:
+        get_node(root["left"],key)
+    else:
+        get_node(root["left"],key)
+        get_node(root["right"], key)
+        
+    return None
 
 def get (my_bst, key):
-    
+    return get_node(my_bst, key)
