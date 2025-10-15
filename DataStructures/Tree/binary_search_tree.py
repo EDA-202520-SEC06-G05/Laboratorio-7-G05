8e95a82bc665f41d4cea556e5487eacdbad8b092
SonderZ27 <simon.alarcon0627@gmail.com>
2025-10-15 14:40:41 -0500

    a1

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -1,5 +1,22 @@
-def new_map ():
-    
-    return {"root": None}
+from DataStructures.Tree import bst_node as bn
 
+
+
+def put(my_bst, key, value):
     
+    for i in my_bst["root"]:
+        if i == None:
+            bn.new_node(key,value)
+        elif i["left"] >=
+    
+    
+    
+    
+    
+    
+    
+    pass
+def get():
+    pass
+def size():
+    pass
