06715179098242dd961055dcd45f1c3882cd7a4f
San-FG <manuelsal5436-gmail.com>
2025-10-21 19:37:38 -0500

    Ingreso de funciones

--- DataStructures/Tree/binary_search_tree.py
+++ DataStructures/Tree/binary_search_tree.py
@@ -1,4 +1,5 @@
 from DataStructures.Tree import bst_node 
+from DataStructures.List import single_linked_list as lt
 
 def new_map ():
     
@@ -16,15 +17,10 @@ def insert_node(root, key, value):
         root["value"] = value
     return root
 
-
 def put(my_bst, key, value):
     my_bst["root"] = insert_node(my_bst["root"], key, value)
     return my_bst
     
-def size_tree(tree):
-    if tree is None:
-        return 0
-    return 1 + size_tree(tree["left"]) + size_tree(tree["right"])
 
 def default_compare (key, element):
     if key == bst_node.get_key(element):
@@ -92,6 +88,7 @@ def get_max_node(root):
             current = current["right"]
         return current["key"]
     
+
 def get_max(my_bst):
     if my_bst["root"] is None:
         return None
@@ -163,3 +160,59 @@ def value_set(my_bst):
     value_set_tree(my_bst["root"], values)
     return values
 
+
+def height_tree(root):
+    if root is None:
+        return 0 
+    else:
+        left_height = height_tree(root["left"])
+        right_height = height_tree(root["right"])
+        return 1 + max(left_height, right_height)
+
+def height (my_bst):
+    if my_bst["root"] is None:
+        return 0
+    else:
+        return height_tree(my_bst["root"])
+
+
+def keys_range(root,key_initial, key_final, all_keys):
+    if  key_initial > key_final or lt.size(all_keys) == 0:
+        return lt.new_list()
+    else:
+        keys_in_range = lt.new_list()
+        for i in range (1,lt.size(all_keys)+1):
+            key = lt.get_element(all_keys, i)
+            if key_initial <= key <= key_final:
+                lt.add_last(keys_in_range, key)
+        return keys_in_range
+
+def keys(my_bst,key_initial, key_final):
+    if my_bst["root"] is None:
+        return lt.new_list()
+    else:
+        all_keys = key_set(my_bst)
+        return keys_range(my_bst["root"],key_initial, key_final, all_keys)
+    
+def values_range(my_bst,key_initial, key_final, all_keys):
+    if  key_initial > key_final or lt.size(all_keys) == 0:
+        return lt.new_list()
+    else:
+        values_in_range = lt.new_list()
+        for i in range (1,lt.size(all_keys)+1):
+            key = lt.get_element(all_keys, i)
+            if key_initial <= key <= key_final:
+                values = get(my_bst, key)
+                lt.add_last(values_in_range, values)
+        return values_in_range
+
+
+def values(my_bst,key_initial, key_final):
+    if my_bst["root"] is None:
+        return lt.new_list()
+    else:
+        
+        all_keys = key_set(my_bst)
+        return values_range(my_bst,key_initial, key_final, all_keys)
+    
+    
